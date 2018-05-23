4225897adc1484dcf449dc3a7217d3cd097d9de0
dee2012 <deegriffin2012@gmail.com>
2018-05-22 20:42:40 -0700

    Add files via upload

---
+++ FF4_VersionC/scripts/nav.js
@@ -0,0 +1,9 @@
+
+ $(document).ready(function(){
+	 $(".navLinks").hide();
+	 $(".toggleBtn").click(function(){
+ 	$(".navLinks").slideToggle("fast");
+ 	});
+ });
+		
+
---
+++ FF4_VersionC/scripts/nav2.js
@@ -0,0 +1,11 @@
+
+      $(document).ready(function(){
+		  $(".menu").hide();
+		   $(".toggle").click(function(){
+            $(".menu").slideToggle("fast");
+			   
+        });
+        
+    });
+    
+   
