185c511cc8fa6010447b26de8dae1e2d1d5287d7
Coder Carrie <carrie.furet@gmail.com>
2018-05-22 22:20:49 -0700

    added sticky footer code

--- scripts/fw4.js
+++ scripts/fw4.js
@@ -1,6 +1,17 @@
-///////////////////////////
-//
-//  font-family: 'Oswald', sans-serif; - headlines
-//  font-family: 'Open Sans Condensed', sans-serif; - copy
-//
-///////////////////////////
+//  STICKY FOOTER
+//////////////////////
+function footerAlign() {
+	$('footer').css('display', 'block');
+	$('footer').css('height', 'auto');
+	var footerHeight = $('footer').outerHeight();
+	$('body').css('padding-bottom', footerHeight);
+	$('footer').css('height', footerHeight);
+}
+
+$(document).ready(function() {
+	footerAlign();
+});
+
+$(window).resize(function() {
+	footerAlign();
+})
